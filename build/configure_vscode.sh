5f9ea1d566d0fd170dc8e479ac20f1009cb199c6
jhmorgan <jonathan.h.morgan@gmail.com>
2025-01-11 19:02:42 +0000

    Added .bashrc configuration to allow users to source the file to ensure that the container's display is set to :102 when working within VSCode.

--- build/configure_vscode.sh
+++ build/configure_vscode.sh
@@ -2,6 +2,8 @@
 
 # Create the .vscode directory and settings.json file
 mkdir -p /workspace/.vscode
+
+# Configure settings.json
 cat <<EOL > /workspace/.vscode/settings.json
 {
     "julia.executablePath": "/usr/bin/julia",
@@ -17,3 +19,11 @@ cat <<EOL > /workspace/.vscode/settings.json
 EOL
 
 echo ".vscode/settings.json configured successfully."
+
+# Create the .bashrc file with DISPLAY setting
+cat <<EOL > /workspace/.vscode/.bashrc
+# Setting Port to be :102 to Support X11 Forward via Xephyr
+export DISPLAY=:102
+EOL
+
+echo ".vscode/.bashrc file created with DISPLAY=:102 setting."
